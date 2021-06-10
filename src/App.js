b44528bbb0c2dff4b7a249f41b08756635bfa4be
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-06-10 18:13:58 +0700

    add cycle toggle reminder

--- src/App.js
+++ src/App.js
@@ -30,10 +30,20 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header title='Task Tracker'/>
-      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Task To Show' }
+      { tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask}  onToggle={toggleReminder} />
+      ) : (
+        'No Task To Show'
+      )}
     </div>
   );
 }
