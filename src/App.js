4e26000fb7564c1fcd60632133ffa70dd6a7cabe
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-06-10 17:50:44 +0700

    add ternary for showing task

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="container">
       <Header title='Task Tracker'/>
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Task To Show' }
     </div>
   );
 }
