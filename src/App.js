02738c9db2b3e1984152a4d50942a266883ec01c
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-06-11 21:41:10 +0700

    add feature update data from backend

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     const getTask = async () => {
-      const queryTask = await fetchTask()
+      const queryTask = await fetchTasks()
 
       setTasks(queryTask)
     }
@@ -19,12 +19,22 @@ function App() {
   }, [])
 
   // Fetch Tasks
-  const fetchTask = async () => {
+  const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
     const data = await res.json()
 
     return data
   }
+
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    
+    return data
+  }
+  
+
 
   // Add Task
   const addTask = async(task) => {
@@ -53,8 +63,22 @@ function App() {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  const toggleReminder = async (id) => {
+    const queryTask = await fetchTask(id)
+ 
+    const updateTask =  { ...queryTask, reminder: (!queryTask.reminder) }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updateTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
   }
 
   return (
