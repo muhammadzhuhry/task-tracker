2828089e019b775db51ab927effe81e447abb8c5
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-06-11 21:53:09 +0700

    modify App with router logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -33,8 +36,6 @@ function App() {
     
     return data
   }
-  
-
 
   // Add Task
   const addTask = async(task) => {
@@ -82,19 +83,28 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header 
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask} 
-      />
-      {showAddTask && <AddTask onAdd={addTask} /> }
-      { tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete=
-        {deleteTask}  onToggle={toggleReminder} />
-      ) : (
-        'No Task To Show'
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header 
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask} 
+        />
+        
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} /> }
+            { tasks.length > 0 ? (
+              <Tasks tasks={tasks} onDelete=
+              {deleteTask}  onToggle={toggleReminder} />
+            ) : (
+              'No Task To Show'
+            )}
+          </>
+        )} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
