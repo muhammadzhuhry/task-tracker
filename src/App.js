627372a13b4505b5b987178e09bd2394a070c467
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-06-11 21:30:42 +0700

    add feature add data from backend

--- src/App.js
+++ src/App.js
@@ -27,11 +27,20 @@ function App() {
   }
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
+  const addTask = async(task) => {
+    task.id = Math.floor(Math.random() * 10000) + 1
 
-    setTasks([...tasks, newTask])
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
   }
 
   // Delete Task
